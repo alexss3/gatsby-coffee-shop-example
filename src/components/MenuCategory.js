07bbeaeb7868bd0fc81fa4d4446278e25e4faf7e
Alex Stultz <stultzat@gmail.com>
2021-07-16 18:25:13 +0200

    Add custom menu preview

--- src/components/MenuCategory.js
+++ src/components/MenuCategory.js
@@ -6,12 +6,13 @@ export default function MenuCategory({ category }) {
     <div className={styles.category}>
       <h2>{category.name}</h2>
       <ul>
-        {category.items.map(item => (
-          <li key={item.name}>
-            <div className={styles.name}>{item.name}</div>
-            <div className={styles.price}>{item.price}</div>
-          </li>
-        ))}
+        {category.items &&
+          category.items.map(item => (
+            <li key={item.name}>
+              <div className={styles.name}>{item.name}</div>
+              <div className={styles.price}>{item.price}</div>
+            </li>
+          ))}
       </ul>
     </div>
   )
